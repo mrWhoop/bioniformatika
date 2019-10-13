b84606df477d2581c6d7d83c9e731567a2d8988b
Robi <modic.robert@gmail.com>
2019-10-13 18:00:17 +0200

    resene naloge do 11

---
+++ rosalindTen.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+dataSet = open("files/rosalind_gc.txt", "r")
+data = dataSet.read().splitlines()
+dataSet.close()
+
+seq = ""
+
+for i in data:
+    seq+=i
+
+workSet = seq.split(">")
+workSet.remove('')
+
+highLabel = ""
+high = 0
+
+for i in workSet:
+    label = i[0:13:1]
+    DNA = i[13::]
+    countAll = 0
+    countCG = 0
+
+    for j in DNA:
+        if j == "C" or j == "G":
+            countCG = countCG + 1
+            countAll = countAll + 1
+        else:
+            countAll = countAll + 1
+
+    perc = countCG/countAll
+
+    if perc > high:
+        high = perc
+        highLabel = label
+
+print(highLabel)
+print(high * 100)
